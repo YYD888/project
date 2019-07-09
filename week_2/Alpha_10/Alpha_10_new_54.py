3d3d41772316e84e3512717b4cc6125dc8c41868
YYD888 <34444761+YYD888@users.noreply.github.com>
2019-07-09 20:16:49 +0800

    Add files via upload

---
+++ week_2/Alpha_10/Alpha_10_new_54.py
@@ -0,0 +1,49 @@
+# coding=utf8
+__author__ = 'wangjp'
+
+import time
+
+import numpy as np
+import pandas as pd
+from FactorModule.FactorBase import FactorBase
+from DataReaderModule.Constants import ALIAS_FIELDS as t
+
+class Factor(FactorBase):
+
+    def __init__(self):
+        super(Factor,self).__init__()
+        self.neutral = True
+        self.factorName = __name__.split('.')[-1]
+        self.needFields = [t.OPEN,t.VWAP,t.CLOSE,t.VOLUME, t.ADJFCT, t.TRDSTAT,t.HIGH,t.LOW]  # 设置需要的字段
+
+    def factor_definition(self):
+        """
+        收集派发指标
+        :return:
+        """
+        s = time.time()
+        needData = self.needData                                # 计算所需数
+        
+        adjVwap = needData[t.VWAP] * needData[t.ADJFCT]
+        Volume = needData[t.VOLUME] 
+        adjClose = needData[t.CLOSE] * needData[t.ADJFCT]
+        adjHigh = needData[t.HIGH]
+        adjLow = needData[t.LOW]
+        adjOpen = needData[t.OPEN] * needData[t.ADJFCT]
+        
+        x_1 = self.calculator.Rank(adjOpen-(self.calculator.Sum(adjVwap,10))/10)
+        x_2 = self.calculator.Rank((adjClose>=adjVwap)*(adjClose-adjVwap))
+        distrib =self.calculator.Wma(x_1 - x_2,5,4)
+        factor =  distrib
+
+        print('factor {0} done with {1} seconds'.format(self.factorName, time.time() - s))
+        return factor
+
+    def run_factor(self):
+        self.run()
+        
+
+
+
+fct = Factor()
+fct.run_factor()
