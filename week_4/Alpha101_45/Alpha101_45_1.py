af9d49fc00d4c316adf7c6f8ea01ba648c69e29b
YYD888 <34444761+YYD888@users.noreply.github.com>
2019-07-23 16:18:04 +0800

    Add files via upload

---
+++ week_4/Alpha101_45/Alpha101_45_1.py
@@ -0,0 +1,51 @@
+# coding=utf8
+__author__ = 'wangjp'
+
+import time
+
+import numpy as np
+import pandas as pd
+from FactorModule.FactorBase import FactorBase
+from DataReaderModule.Constants import ALIAS_FIELDS as t
+
+class Factor(FactorBase):
+
+    def __init__(self):
+        super(Factor,self).__init__()
+        self.neutral = True
+        self.factorName = __name__.split('.')[-1]
+        self.needFields = [t.OPEN,t.VWAP,t.CLOSE,t.VOLUME, t.ADJFCT, t.TRDSTAT,t.HIGH,t.LOW,t.PCTCHG,t.MKTCAP]  # 设置需要的字段
+
+    def factor_definition(self):
+        """
+        收集派发指标
+        :return:
+        """
+        s = time.time()
+        needData = self.needData                                # 计算所需数
+
+        adjVwap = needData[t.VWAP] * needData[t.ADJFCT]
+        Volume = needData[t.VOLUME] 
+        adjClose = needData[t.CLOSE] * needData[t.ADJFCT]
+        adjHigh = needData[t.HIGH]
+        adjLow = needData[t.LOW]
+        adjOpen = needData[t.OPEN]
+        adjReturn = needData[t.PCTCHG]
+        
+        x_1 = self.calculator.Rank(self.calculator.Sum(self.calculator.Delay(adjClose, 5),10)/10)
+        x_2 = self.calculator.Corr(adjClose, Volume, 10)
+        x_3 = self.calculator.Rank(self.calculator.Corr(self.calculator.Sum(adjClose,5), self.calculator.Sum(adjClose,10), 6)) 
+        
+        factor = -x_1*x_2*x_3/self.calculator.Std(adjClose,9)
+        
+        print('factor {0} done with {1} seconds'.format(self.factorName, time.time() - s))
+        return factor
+
+    def run_factor(self):
+        self.run()
+        
+
+
+
+fct = Factor()
+fct.run_factor()
